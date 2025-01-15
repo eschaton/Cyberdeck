b670a5ca55735dc744a27af6e983506ff1d2c074
Chris Hanson <cmhanson@eschatologist.net>
2025-01-14 21:57:26 -0800

    Set up the thread for the PP.

--- Cyber/Cyber962PP.c
+++ Cyber/Cyber962PP.c
@@ -22,10 +22,14 @@
 #include "Cyber962PPInstructions.h"
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 
 CYBER_SOURCE_BEGIN
+
+
+static void * _Nullable Cyber962PPThread(void * _Nullable ppv);
 
 
 struct Cyber962PP * _Nullable Cyber962PPCreate(struct Cyber962IOU *inputOutputUnit, int index)
@@ -40,6 +44,20 @@ struct Cyber962PP * _Nullable Cyber962PPCreate(struct Cyber962IOU *inputOutputUn
 
     pp->_storage = calloc(8192, sizeof(CyberWord16));
 
+    int lock_err = pthread_mutex_init(&pp->_lock, NULL);
+    if (lock_err != 0) {
+        assert(lock_err != 0); // halt here in debug builds
+        Cyber962PPDispose(pp);
+        return NULL;
+    }
+
+    int cond_err = pthread_cond_init(&pp->_condition, NULL);
+    if (cond_err != 0) {
+        assert(cond_err != 0); // halt here in debug builds
+        Cyber962PPDispose(pp);
+        return NULL;
+    }
+
     pp->_instructionCache = calloc(65536, sizeof(void *));
 
     Cyber962PPReset(pp);
@@ -50,9 +68,16 @@ struct Cyber962PP * _Nullable Cyber962PPCreate(struct Cyber962IOU *inputOutputUn
 
 void Cyber962PPDispose(struct Cyber962PP * _Nullable pp)
 {
+    assert(pp->_running == false);
+
     if (pp == NULL) return;
 
     free(pp->_storage);
+
+    // pp->_thread is cleaned up by exit
+    pthread_mutex_destroy(&pp->_lock);
+    pthread_cond_destroy(&pp->_condition);
+
     free(pp->_instructionCache);
 
     free(pp);
@@ -61,9 +86,70 @@ void Cyber962PPDispose(struct Cyber962PP * _Nullable pp)
 
 void Cyber962PPReset(struct Cyber962PP *pp)
 {
-    pp->_regA = 010000;
-    pp->_regP = 0x0001;
+    pp->_regA = 0010000;
+    pp->_regP = 0000001;
     pp->_regR = 0x00000000;
+}
+
+
+void Cyber962PPStart(struct Cyber962PP *pp)
+{
+    assert(pp != NULL);
+
+    int thread_err = pthread_create(&pp->_thread, NULL, Cyber962PPThread, pp);
+    if (thread_err != 0) {
+        assert(thread_err != 0); // halt here in debug builds
+        Cyber962PPDispose(pp);
+        return;
+    }
+}
+
+void Cyber962PPStop(struct Cyber962PP *pp)
+{
+    assert(pp != NULL);
+
+    pthread_mutex_lock(&pp->_lock); {
+        pp->_running = false;
+    } pthread_mutex_unlock(&pp->_lock);
+}
+
+/// The main loop for a Peripheral Processor, which runs a single step of its execution.
+void Cyber962PPSingleStep(struct Cyber962PP *processor)
+{
+    assert(processor != NULL);
+
+    CyberWord16 oldP = processor->_regP;
+    union Cyber962PPInstructionWord instructionWord;
+    instructionWord._raw = Cyber962PPReadSingle(processor, oldP);
+    Cyber962PPInstruction instruction = Cyber962PPInstructionDecode(processor, instructionWord, oldP);
+    CyberWord16 advance = instruction(processor, instructionWord);
+    CyberWord16 newP = oldP + advance;
+    processor->_regP = newP;
+}
+
+/// The thread function for the main loop for a Peripheral Processor.
+static void * _Nullable Cyber962PPThread(void * _Nullable ppv)
+{
+    struct Cyber962PP *pp = (struct Cyber962PP *)ppv;
+
+    // Disable cancellation.
+
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+    // Do work in a loop.
+
+    bool running = true;
+    while (running) {
+        // Run the main loop once.
+        Cyber962PPSingleStep(pp);
+
+        // Check for a stop request.
+        pthread_mutex_lock(&pp->_lock); {
+            running = pp->_running;
+        } pthread_mutex_unlock(&pp->_lock);
+    }
+
+    return NULL;
 }
 
 
@@ -112,20 +198,6 @@ void Cyber962PPWriteMultiple(struct Cyber962PP *processor, CyberWord16 address, 
     for (CyberWord16 i = 0; i < count; i++) {
         processor->_storage[address + i] = buffer[i];
     }
-}
-
-
-void Cyber962PPSingleStep(struct Cyber962PP *processor)
-{
-    assert(processor != NULL);
-
-    CyberWord16 oldP = processor->_regP;
-    union Cyber962PPInstructionWord instructionWord;
-    instructionWord._raw = Cyber962PPReadSingle(processor, oldP);
-    Cyber962PPInstruction instruction = Cyber962PPInstructionDecode(processor, instructionWord, oldP);
-    CyberWord16 advance = instruction(processor, instructionWord);
-    CyberWord16 newP = oldP + advance;
-    instructionWord._raw = newP;
 }
 
 
